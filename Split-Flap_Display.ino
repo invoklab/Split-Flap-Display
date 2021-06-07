c2266400ec4f97175eb7c16666b3089045e68120
Thoby Noorhalim <thoby.noorhalim@outlook.com>
2021-06-07 15:06:30 +0800

    First commit

---
+++ Split-Flap_Display.ino
@@ -0,0 +1,124 @@
+//Includes the Arduino Stepper Library
+#include <Stepper.h>
+#include <Unistep2.h>
+
+// Defines the number of steps per rotation
+const int stepsPerRevolution = 2048;
+int sensor = A0;
+int val;
+int currentPos;
+int zeroPos;
+int magPos;
+bool magDetected = false;
+int stepsToA = 1250;
+int stepsLeft = 0;
+int setSteps = 0;
+int homingOffset = 1100;
+bool homingDone = false;
+int incomingByte = 0;
+char letter;
+bool printable = false;
+
+// Creates an instance of stepper class
+// Pins entered in sequence IN1-IN3-IN2-IN4 for proper step sequence
+//Stepper myStepper = Stepper(stepsPerRevolution, 4, 6, 5, 7);
+Unistep2 myStepper(4,5,6,7, 4096, 1000);
+
+void homing(){
+    Serial.println("Homing ...");
+    val = analogRead(sensor);
+
+    while(val < 590){
+        myStepper.move(4096);
+        myStepper.run();
+        val = analogRead(sensor);
+    }
+
+    // Magnetic field detected, set this flap as 0 position
+    // Pull down stepper shield
+    magDetected = true;
+    magPos = myStepper.currentPosition();
+
+    // Move flaps to designated start flaps ( one flap before A)
+    myStepper.move(homingOffset);
+    stepsLeft = myStepper.stepsToGo();
+    // Serial.println(magPos);
+    while(stepsLeft != 0){
+        myStepper.run();
+        stepsLeft = myStepper.stepsToGo();
+    }
+
+    myStepper.stop();
+
+    // Set homing done flag
+    homingDone = true;
+    Serial.println("Homing Done !");
+}
+
+void goToLetter(char letter){
+    homing();
+    setSteps = (letter - 'a' + 1)*137;
+    Serial.println("Set steps is "+(String)setSteps);
+
+    myStepper.move(setSteps);
+    stepsLeft = myStepper.stepsToGo();
+    Serial.println("Steps to go "+(String)stepsLeft);
+    while(stepsLeft > 0){
+        myStepper.run();
+        stepsLeft = myStepper.stepsToGo();
+    }
+
+    Serial.println("Function called, steps to go "+(String)stepsLeft);
+    myStepper.stop();
+}
+
+void setup() {
+  // Nothing to do (Stepper Library sets pins as outputs)
+  Serial.begin(9600);
+//  myStepper.setSpeed(10);
+  pinMode(sensor, INPUT);
+  
+}
+
+void loop() {
+    if(!homingDone){
+        homing();
+    } 
+
+    // Read serial
+    if(Serial.available() > 0){
+        // read byte
+        incomingByte = Serial.read();
+
+        // Check if letter entered can be displayed
+        // Check based on ASCII table
+        if(incomingByte < 'A' || incomingByte > 'z' || 
+            (incomingByte >= 91 && incomingByte <= 96)){
+            // can't be displayed, ignore letter
+            Serial.println("ERROR, letter can't be displayed");
+            !printable;
+        }
+        else if(incomingByte >= 'A' && incomingByte <= 'Z'){
+            // uppercase detected, convert to lowercase
+            incomingByte = incomingByte + 32;
+            printable = true;
+        } 
+        else if (incomingByte == ' '){
+            incomingByte = incomingByte + 'z' + 1;
+            printable = true;
+        }
+        
+        if(printable){
+            Serial.println("Incoming byte is "+(String)incomingByte);
+
+            letter = (char)incomingByte;
+
+            Serial.println("Entered letter is "+(String)letter);
+
+            goToLetter(letter);
+            delay(500);
+        }    
+        
+    }
+
+}
